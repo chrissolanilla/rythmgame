d24be67680d608caec245712a33d0c7994d709e8
Triple-A17 <andresabrams17@gmail.com>
2025-11-14 19:26:49 -0500

    left for pizza

--- MenuScene/song_selections.gd
+++ MenuScene/song_selections.gd
@@ -1,188 +1,139 @@
 extends Control
-@onready var difficultyButton: Button = $Button
 
+# ----------------------------
+# Node references
+# ----------------------------
+@onready var difficultyButton: Button = $Button
 @onready var Next: Button = $"next"
+@onready var Prev: Button = $"prev"
+@onready var TextureRectNode: TextureRect = $TextureRect
+@onready var SongLabel: RichTextLabel = $RichTextLabel
+
+# ----------------------------
+# Lifecycle
+# ----------------------------
 func _ready() -> void:
 	GlobalSettings.test_play = false
 	Next.grab_focus()
-	var config = ConfigFile.new()
-	var err = config.load("user://Settings.cfg")
-	
-	if err == OK:
-		var chartPathList = config.get_value("game", "chartPath", [])
-		var chartImageList = config.get_value("game", "chartImage", [])
-		var chartSongList = config.get_value("game", "chartSong", [])
-		var chartIndex = GlobalSettings.startingIndex
-		var loadedImage = chartImageList[chartIndex]
-		var loadedSong = parse_filename(chartSongList[chartIndex])
-		GlobalSettings.startingChartPath = chartPathList[chartIndex]
-		GlobalSettings.current_song = chartSongList[chartIndex]
-		GlobalSettings.startingChartImage = chartImageList[chartIndex]
-		print("chart song is set to ", chartSongList[chartIndex])
-		print("chart is set to : ", chartPathList[chartIndex])
-		$TextureRect.texture = load(loadedImage)
-		$RichTextLabel.text = loadedSong
 
+	_update_song_display()
 	_update_difficulty_button()
+	_update_difficulty_button_color()
 
-#func _ready() -> void:
-	#GlobalSettings.test_play = false
-	#Next.grab_focus()
-	#var config = ConfigFile.new()
-	#var err = config.load("user://Settings.cfg")
-	#
-	#if err == OK:
-		#var chartPathList = config.get_value("game", "chartPath", [])
-		#var chartImageList = config.get_value("game", "chartImage", [])
-		#var chartSongList = config.get_value("game", "chartSong", [])
-		#var chartIndex = GlobalSettings.startingIndex
-		#var loadedImage = chartImageList[chartIndex]
-		#var loadedSong = parse_filename(chartSongList[chartIndex])
-		#GlobalSettings.startingChartPath =  chartPathList[chartIndex]
-		#GlobalSettings.current_song = chartSongList[chartIndex]
-		#GlobalSettings.startingChartImage = chartImageList[chartIndex]
-		#print("chart song is set to ", chartSongList[chartIndex])
-		#print("chart is set to : ", chartPathList[chartIndex])
-		#$TextureRect.texture = load(loadedImage)
-		#$RichTextLabel.text = loadedSong
-		
-	
+	if not difficultyButton.is_connected("pressed", Callable(self, "_on_difficulty_pressed")):
+		difficultyButton.connect("pressed", Callable(self, "_on_difficulty_pressed"))
 
+# ----------------------------
+# Play / Menu buttons
+# ----------------------------
 func _on_play_map_pressed() -> void:
 	get_tree().change_scene_to_file("res://9ArrowScene/Game.tscn")
-	pass # Replace with function body.
-
-
-func _on_next_pressed() -> void:
-	GlobalSettings.startingChartPath = get_Next_Song()
-	print("Chart set to ",GlobalSettings.startingChartPath)
-	pass # Replace with function body.
-
-
-func _on_prev_pressed() -> void:
-	GlobalSettings.startingChartPath = get_Prev_Song()
-	print("Chart set to ",GlobalSettings.startingChartPath) # Replace with function body.
-	pass # Replace with function body.
-	
-func get_Prev_Song() -> String:
-	var prevSongChart: String = ""
-	var prevSongName: String = ""
-	var prevChartImage: String = ""
-	var newIndex = 0
-	var config = ConfigFile.new()
-	var err = config.load("user://Settings.cfg")
-
-	if err == OK:
-		var chartList = config.get_value("game", "chartPath", [])
-		var songNameList = config.get_value("game", "chartSong", [])
-		var chartImageList = config.get_value("game", "chartImage", [])
-		# Read values from the file (with defaults in case they are missing)
-		if(GlobalSettings.startingIndex == 0):
-			newIndex = len(chartList)-1
-			GlobalSettings.startingIndex = newIndex
-			prevSongChart = chartList[GlobalSettings.startingIndex]
-			prevSongName = songNameList[GlobalSettings.startingIndex]
-			prevChartImage = chartImageList[GlobalSettings.startingIndex]
-			GlobalSettings.startingChartImage = prevChartImage
-		else:
-			newIndex = GlobalSettings.startingIndex-1
-			GlobalSettings.startingIndex = newIndex
-			prevSongChart = chartList[GlobalSettings.startingIndex]
-			prevSongName = songNameList[GlobalSettings.startingIndex]
-			prevChartImage = chartImageList[GlobalSettings.startingIndex]
-			GlobalSettings.startingChartImage = prevChartImage
-	$TextureRect.texture = load(prevChartImage)
-	$RichTextLabel.text = parse_filename(prevSongName)
-
-	GlobalSettings.current_song = prevSongName
-	print("song is now : ", prevSongName)
-
-	# NEW: reset difficulty to first option for this song
-	GlobalSettings.currentDifficultyIndex = 0
-	GlobalSettings._update_current_difficulty()
-	_update_difficulty_button()
-
-	return prevSongChart
-
-
-	
-func get_Next_Song() -> String:
-	var nextSongChart: String = ""
-	var nextSongName: String = ""
-	var nextChartImage: String = ""
-	var newIndex = 0
-	var config = ConfigFile.new()
-	var err = config.load("user://Settings.cfg")
-
-	if err == OK:
-		var chartList = config.get_value("game", "chartPath", [])
-		var songNameList = config.get_value("game", "chartSong", [])
-		var chartImageList = config.get_value("game", "chartImage", [])
-		# Read values from the file (with defaults in case they are missing)
-		if(GlobalSettings.startingIndex == (len(chartList)-1)):
-			newIndex = 0
-			GlobalSettings.startingIndex = newIndex
-			nextSongChart = chartList[GlobalSettings.startingIndex]
-			nextSongName = songNameList[GlobalSettings.startingIndex]
-			nextChartImage = chartImageList[GlobalSettings.startingIndex]
-			GlobalSettings.startingChartImage = nextChartImage
-		else:
-			newIndex = GlobalSettings.startingIndex+1
-			GlobalSettings.startingIndex = newIndex
-			nextSongChart = chartList[GlobalSettings.startingIndex]
-			nextSongName = songNameList[GlobalSettings.startingIndex]
-			nextChartImage = chartImageList[GlobalSettings.startingIndex]
-			GlobalSettings.startingChartImage = nextChartImage
-	$RichTextLabel.text = parse_filename(nextSongName)
-	$TextureRect.texture = load(nextChartImage)
-	GlobalSettings.current_song = nextSongName
-	print("song is now ", nextSongName)
-
-
-	# NEW: reset difficulty to first option for this song
-	GlobalSettings.currentDifficultyIndex = 0
-	GlobalSettings._update_current_difficulty()
-	_update_difficulty_button()
-
-	return nextSongChart
-
-func parse_filename(path: String) -> String:
-	var prefix = "res://mp3files/"
-	var suffix = ".mp3"
-	
-	# Remove the prefix if present
-	if path.begins_with(prefix):
-		path = path.substr(prefix.length())
-	
-	# Handle case-insensitive .mp3 suffix
-	var lower_path = path.to_lower()
-	if lower_path.ends_with(suffix):
-		path = path.substr(0, path.length() - suffix.length())
-	return path
-
 
 func _on_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://MenuScene/Menu.tscn")
-	pass # Replace with function body.
 
+# ----------------------------
+# Next / Previous Songs
+# ----------------------------
+func _on_next_pressed() -> void:
+	GlobalSettings.startingChartPath = get_Next_Song()
+	_update_song_display()
+	_update_difficulty_button()
+	_update_difficulty_button_color()
 
-func _on_button_pressed() -> void:
-	
-	pass # Replace with function body.
+func _on_prev_pressed() -> void:
+	GlobalSettings.startingChartPath = get_Prev_Song()
+	_update_song_display()
+	_update_difficulty_button()
+	_update_difficulty_button_color()
+
+# ----------------------------
+# Difficulty button pressed
+# ----------------------------
+func _on_difficulty_pressed() -> void:
+	GlobalSettings.cycle_difficulty()
+	_update_difficulty_button()
+	_update_difficulty_button_color()
+
+# ----------------------------
+# Update display helpers
+# ----------------------------
+func _update_song_display() -> void:
+	TextureRectNode.texture = load(GlobalSettings.startingChartImage)
+	SongLabel.text = parse_filename(GlobalSettings.current_song)
 
 func _update_difficulty_button() -> void:
-	var diffs = GlobalSettings.chartDifficulties
-	if diffs.is_empty():
-		difficultyButton.text = "N/A"
-		return
+	difficultyButton.text = GlobalSettings.difficulty
 
-	var song_idx = clamp(GlobalSettings.startingIndex, 0, diffs.size() - 1)
-	var song_diffs = diffs[song_idx]
-	if not (song_diffs is Array) or song_diffs.is_empty():
-		difficultyButton.text = "N/A"
-		return
+func _update_difficulty_button_color() -> void:
+	var color = Color.GRAY
+	match GlobalSettings.difficulty:
+		"Easy": color = Color(0,1,0)
+		"Medium": color = Color(1,1,0)
+		"Hard": color = Color(1,0,0)
+		_: color = Color.GRAY
 
-	var idx = clamp(GlobalSettings.currentDifficultyIndex, 0, song_diffs.size() - 1)
-	GlobalSettings.currentDifficultyIndex = idx
-	GlobalSettings.currentDifficulty = str(song_diffs[idx])
-	difficultyButton.text = GlobalSettings.currentDifficulty
+	var stylebox = StyleBoxFlat.new()
+	stylebox.bg_color = color
+	difficultyButton.add_theme_stylebox_override("normal", stylebox)
+	difficultyButton.add_theme_stylebox_override("hover", stylebox)
+	difficultyButton.add_theme_stylebox_override("pressed", stylebox)
+
+# ----------------------------
+# Song cycling
+# ----------------------------
+func get_Next_Song() -> String:
+	GlobalSettings.song_index = (GlobalSettings.song_index + 1) % GlobalSettings.songs.size()
+	GlobalSettings.current_song_dict = GlobalSettings.songs[GlobalSettings.song_index]
+
+	var charts = GlobalSettings.current_song_dict.get("charts", {})
+	var ordered = ["Easy","Medium","Hard"]
+	var diffs = []
+	for d in ordered:
+		if d in charts.keys():
+			diffs.append(d)
+
+	GlobalSettings.currentDifficultyIndex = 0
+	GlobalSettings.difficulty = diffs[0] if diffs.size() > 0 else "None"
+	GlobalSettings.startingChartPath = charts.get(GlobalSettings.difficulty,"")
+	GlobalSettings.startingChartImage = GlobalSettings.current_song_dict.get("image","")
+	GlobalSettings.current_song = GlobalSettings.current_song_dict.get("audio","")
+	GlobalSettings.startingIndex = GlobalSettings.song_index
+
+	GlobalSettings._update_starting_chart_array()
+	GlobalSettings.save_config()
+	return GlobalSettings.startingChartPath
+
+func get_Prev_Song() -> String:
+	GlobalSettings.song_index = (GlobalSettings.song_index - 1 + GlobalSettings.songs.size()) % GlobalSettings.songs.size()
+	GlobalSettings.current_song_dict = GlobalSettings.songs[GlobalSettings.song_index]
+
+	var charts = GlobalSettings.current_song_dict.get("charts", {})
+	var ordered = ["Easy","Medium","Hard"]
+	var diffs = []
+	for d in ordered:
+		if d in charts.keys():
+			diffs.append(d)
+
+	GlobalSettings.currentDifficultyIndex = 0
+	GlobalSettings.difficulty = diffs[0] if diffs.size() > 0 else "None"
+	GlobalSettings.startingChartPath = charts.get(GlobalSettings.difficulty,"")
+	GlobalSettings.startingChartImage = GlobalSettings.current_song_dict.get("image","")
+	GlobalSettings.current_song = GlobalSettings.current_song_dict.get("audio","")
+	GlobalSettings.startingIndex = GlobalSettings.song_index
+
+	GlobalSettings._update_starting_chart_array()
+	GlobalSettings.save_config()
+	return GlobalSettings.startingChartPath
+
+# ----------------------------
+# Helper: parse filename
+# ----------------------------
+func parse_filename(path: String) -> String:
+	var prefix = "res://mp3files/"
+	var suffix = ".mp3"
+	if path.begins_with(prefix):
+		path = path.substr(prefix.length())
+	if path.to_lower().ends_with(suffix):
+		path = path.substr(0, path.length() - suffix.length())
+	return path
